8696751b0771dfa517a59198c5800cafc905babd
Valerii <valeriy.stronskiy@gmail.com>
2023-10-30 13:39:53 +0200

    Task Done

---
+++ Bank.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+namespace Multithreading__Class_Bank_
+{
+    internal class Bank
+    {
+        private int money;
+        private int percent;
+        private string name;
+        private readonly string fileName;
+
+        public Bank(int money, int percent, string name, string fileName = "BankData")
+        {
+            this.money = money;
+            this.percent = percent;
+            this.name = name;
+            this.fileName = fileName;
+        }
+
+        public int Money
+        {
+            get 
+            { 
+                return money; 
+            }
+            set
+            {
+                DedicateThread(fileName);
+
+                this.money = value;
+            }
+        }
+        public int Percent
+        {
+            get 
+            { 
+                return percent; 
+            }
+            set
+            {
+                DedicateThread(fileName);
+
+                this.percent = value;
+            }
+        }
+        public string Name
+        {
+            get 
+            { 
+                return name; 
+            }
+            set
+            {
+                DedicateThread(fileName);
+
+                this.name = value;
+            }
+        }
+
+        private void DedicateThread(object fileName)
+        {
+            Thread dedicatedThread = new Thread(new ParameterizedThreadStart(SaveDataToFile)) { IsBackground = true };
+            dedicatedThread.Start((string)fileName);
+            dedicatedThread.Join();
+        }
+
+        private void SaveDataToFile(object item)
+        {
+            try
+            {
+                if (item is String fileName)
+                {
+                    using (StreamWriter sw = new StreamWriter($"../../{fileName}.txt", true))
+                    {
+                        sw.WriteLine($" Saved at: {DateTime.Now.ToLocalTime()}");
+                        sw.WriteLine($" Name: {name}");
+                        sw.WriteLine($" Money: {money}");
+                        sw.WriteLine($" Percent: {percent}");
+                        sw.WriteLine("---------------------");
+
+                        Console.WriteLine($"\n All previous data was saved to {fileName}.txt");
+                    }
+                }
+                else
+                {
+                    throw new Exception($" Data type {item.GetType().Name} is not supported!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($" Exception Message: {ex.Message}");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Multithreading__Class_Bank_
 {
@@ -10,6 +13,121 @@ namespace Multithreading__Class_Bank_
     {
         static void Main(string[] args)
         {
+            int money = 0;
+            int percent = 0;
+            Bank bank = null;
+            char choice = ' ';
+            string name = null;
+
+            do
+            {
+                Console.Clear();
+                if (string.IsNullOrEmpty(name))
+                {
+                    Console.WriteLine("\n No information has been added yet!");
+                }
+                else
+                {
+                    Console.WriteLine("\n Data that is currently available:\n");
+                    Console.WriteLine($"  Name: {name}");
+                    Console.WriteLine($"  Percent: {percent}%");
+                    Console.WriteLine($"  Money: {money} UAH");
+                }
+
+                Console.WriteLine("\n Select an option:\n");
+                Console.WriteLine("  1. Add/Edit information;");
+                Console.WriteLine("  0. End session;\n");
+
+                Console.Write("\n Enter your choice: ");
+                choice = Console.ReadKey(false).KeyChar;
+
+                Console.Clear();
+                switch (choice)
+                {
+                    case '1':
+                        if(string.IsNullOrEmpty(name))
+                        {
+                            Console.Write("\n Enter name: ");
+                            name = Console.ReadLine();
+
+                            while (!CheckNumberInput(out percent, "percent"));
+                            while (!CheckNumberInput(out money, "money"));
+
+                            bank = new Bank(money, percent, name);
+                        }
+                        else
+                        {
+                            char subChoice = ' ';
+                            do
+                            {
+                                Console.WriteLine("\n Choose field to edit:\n");
+                                Console.WriteLine("  1. Name;");
+                                Console.WriteLine("  2. Percent;");
+                                Console.WriteLine("  3. Money;");
+                                Console.WriteLine("  0. Return to main menu;\n");
+
+                                Console.Write("\n Enter your choice: ");
+                                subChoice = Console.ReadKey(false).KeyChar;
+
+                                Console.Clear();
+                                switch (subChoice)
+                                {
+                                    case '1':
+                                        Console.Write("\n Enter name: ");
+                                        bank.Name = Console.ReadLine();
+                                        break;
+
+                                    case '2':
+                                        while (!CheckNumberInput(out percent, "percent"));
+                                        bank.Percent = percent;
+                                        break;
+
+                                    case '3':
+                                        while (!CheckNumberInput(out money, "money")) ;
+                                        bank.Money = money;
+                                        break;
+
+                                    case '0':
+                                        Console.WriteLine("\n Returning to main menu...\n");
+                                        break;
+
+                                    default:
+                                        Console.WriteLine($"\n Invalid character \"{subChoice}\", please re-enter!");
+                                        Console.WriteLine("----------------------------------------");
+                                        break;
+                                }
+
+                            } while (subChoice != '0');
+                        }
+
+                        break;
+
+                    case '0':
+                        Console.WriteLine("\n The application is closing...\n\n");
+                        return;
+
+                    default:
+                        Console.WriteLine($"\n Invalid character \"{choice}\", please re-enter!");
+                        Console.WriteLine("----------------------------------------");
+                        break;
+                }
+            } while (choice != '0');
+        }
+
+        static bool CheckNumberInput(out int number, string numberName)
+        {
+            Console.Write($" Enter {numberName}: ");
+
+            if (Int32.TryParse(Console.ReadLine(), out number))
+            {
+                return true;
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine($"\n Incorrect input, try again!");
+                return false;
+            }
         }
     }
 }
